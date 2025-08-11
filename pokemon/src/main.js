06536fa308c359d49280667b9bc9fa521656911f
iamnabil <23001886@siswa.um.edu.my>
2025-08-11 17:42:10 +0800

    load all sprites

--- pokemon/src/main.js
+++ pokemon/src/main.js
@@ -1,11 +1,58 @@
 import kaplay from "kaplay";
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+const k = kaplay({
+    width : 1280,
+    height : 720,
+    scale : 0.7,
+});
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+k.loadSpriteAtlas("sprites/characters.png", {
+    'player-down' : { x: 0, y: 82, width: 16, height: 16},
+    'player-up' : { x: 16, y: 82, width: 16, height: 16},
+    'player-side' : { x: 0, y: 98, width: 32, height: 16, sliceX: 2, sliceY: 1,
+        anims: { 'walk' : {from: 0, to : 1,speed: 6}}
+    },
+    'npc' : { x: 32, y: 98, width: 16, height: 16},
+    'cat-mon' : { x: 0, y: 16, width: 32, height: 32},
+    'spider-mon' : { x: 32, y: 16, width: 32, height: 32},
+    'centipede-mon' : { x: 64, y: 16, width: 32, height: 32},
+    'grass-mon' : { x: 0, y: 49, width: 32, height: 32},
+    'mushroom-mon' : { x: 32, y: 49, width: 32, height: 32},
+    'mini-mon' : { x: 0, y: 0, width: 128, height: 16, sliceX : 8, sliceY: 1,
+        anims: {'spider': 1, 'centipede': 2, 'grass' : 3}
+    },
+})
+k.loadSpriteAtlas("sprites/tiles.png", {
+    'tile' : {x: 0, y:0 , width : 128, height: 128, sliceX: 8, sliceY : 8,
+        anims: {
+            'bigtree-pt1' : 1,
+            'bigtree-pt2' : 2,
+            'bigtree-pt3' : 9,
+            'bigtree-pt4' : 10,
+            'grass-m' : 14,
+            'grass-tl' : 17,
+            'grass-tm' : 18,
+            'grass-tr' : 19,
+            'grass-l' : 25,
+            'grass-r' : 27,
+            'grass-bl' : 33,
+            'grass-mb' : 34,
+            'grass-br' : 35,
+            'tree-t' : 4,
+            'tree-b' : 12,
+            'grass-water' : 20,
+            'sand-1' : 6,
+            'ground-l' : 41,
+            'ground-m' : 42,
+            'ground-r' : 43,
+            'rock-water' : 60
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+
+        }
+    }
+})
+
 
 k.onClick(() => k.addKaboom(k.mousePos()));
